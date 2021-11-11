99d7393857b386e7cdd9fad405977d578ae67ba8
xingchensong <xingchensong1996@163.com>
2021-11-11 14:42:26 +0800

    update install_pkg.sh

--- scripts/install_pkg.sh
+++ scripts/install_pkg.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 # Xingchen Song @ 2020-12-30
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python3-pip clang ctags zsh
+# sudo apt-get update
+# sudo apt-get upgrade
+# sudo apt-get install python3-pip clang ctags zsh
+# # set priority
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 3
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+# sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 3
+# sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+#
+# sudo update-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.7 3
+# sudo update-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.5 1
+# sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3.7 3
+# sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip2.7 1
 
-# set priority
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 3
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 3
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-
-sudo update-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.7 3
-sudo update-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.5 1
-sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3.7 3
-sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip2.7 1
-
+# install miniconda3-py3.8
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh
 # update vim (>= 8.2)
-
-pip3 install --user pynvim neovim jedi autopep8
+conda update -n base conda -y && conda install -y pip
+conda install -c conda-forge vim -y
+pip3 install --user pynvim neovim jedi autopep8 cpplint
