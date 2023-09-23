9874993a9d4f31702dbb82950cffc828a645d60a
xingchensong <xingchensong1996@163.com>
2023-09-23 13:52:10 +0800

    feat(debug): Add vimspector

--- scripts/install_pkg.sh
+++ scripts/install_pkg.sh
@@ -15,11 +15,15 @@
 # sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3.7 3
 # sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip2.7 1
 
-# install miniconda3-py3.8
-wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh
+# install miniconda3-py3.x
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 # update vim (>= 9.0.1425) nodejs (>= 16.13.1) for copilot, tmux (>= 3.3) for xx (?), cmake (>=3.10.1) for c++ projects
+# # https://anaconda.org/conda-forge/vim
+# # https://anaconda.org/conda-forge/cmake
+# # https://anaconda.org/conda-forge/tmux
+# # https://anaconda.org/conda-forge/nodejs
 conda update -n base conda -y && conda install -y pip
-conda install -c conda-forge vim=9.0.1425 zsh shellcheck tmux=3.3 cmake=3.22.1 nodejs=16.13.1 ruby gawk -y
+conda install -c conda-forge vim zsh shellcheck tmux cmake nodejs ruby gawk -y
 conda search cudatoolkit
 conda install cudatoolkit
 pip3 install pynvim neovim jedi autopep8 cpplint pylint isort cmakelint cmake-format flake8 -i https://pypi.tuna.tsinghua.edu.cn/simple
