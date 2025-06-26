78f55234af483c5ebaaca6d944208c2bbbe1a280
xingchensong <xingchensong1996@163.com>
2025-06-26 17:10:42 +0800

    add docker

---
+++ docker/touchnet/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "# [$(whoami)@$(hostname):$(pwd)]"
+echo "# [$(date +%Y-%m-%d\ %T)] [PID: $$] Running: $0 $@"
+set -e -o pipefail -u
+
+docker build -t micr.cloud.mioffice.cn/base_model/touchnet:1.0.0 .
+
+echo "# [$(date +%Y-%m-%d\ %T)] [PID: $$] Finished: $0 $@"
+exit 0
